01b04fe6563b7afb4f768e1200ca3e56583d9cec
s27542 <s27542@pjwstk.edu.pl>
2024-06-07 14:34:04 +0200

    changess

--- CodeFirst/CodeFirst/Models/BookAward.cs
+++ CodeFirst/CodeFirst/Models/BookAward.cs
@@ -10,8 +10,8 @@ public class BookAward
     public int Year { get; set; }
     public int BookId { get; set; }
     public int AwardId { get; set; }
-    [ForeignKey(nameof(BookId))]
     
+    [ForeignKey(nameof(BookId))]
     public Book Book { get; set; } = null!;
     [ForeignKey(nameof(AwardId))]
     public Award Award { get; set; } = null!;
